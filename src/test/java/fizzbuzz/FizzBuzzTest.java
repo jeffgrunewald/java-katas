f5e1fed246a1c8ec2f7001275f19d502f61298e6
jeffgrunewald <jeff@grunewalddesign.com>
2017-12-07 17:56:17 -0500

    Failing on 3/fizz

--- src/test/java/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/fizzbuzz/FizzBuzzTest.java
@@ -16,4 +16,14 @@ public class FizzBuzzTest {
         FizzBuzz tester = new FizzBuzz(asList(1));
         assertEquals(asList("1"), tester.convert());
     }
+    @Test
+    public void supplyTwoIntsGetTwoInts() {
+        FizzBuzz tester2 = new FizzBuzz(asList(1, 2));
+        assertEquals(asList("1", "2"), tester2.convert());
+    }
+    @Test
+    public void supplyThreeGetFizz() {
+        FizzBuzz tester3 = new FizzBuzz(asList(3));
+        assertEquals(asList("fizz"), tester3.convert());
+    }
 }
